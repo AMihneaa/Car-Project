b29ac9e3d7928bd93582fcbadf9facb46065c049
Mihnea <asmihnea5@gmail.com>
2023-08-05 12:45:42 +0300

    Initial commit

---
+++ src/test/java/com/project/CarDB/CarDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.CarDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
